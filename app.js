9de226f87660ca08aa8316ff3f7077e5eea1fdcc
Alexey Borisov <a@borisco.ru>
2019-08-07 11:59:59 +0300

    changed the port number

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3100;
 const Book = require('./models/bookModel');
 const bookRouter = require('./routes/bookRouter')(Book);
 
