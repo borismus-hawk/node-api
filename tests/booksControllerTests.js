c657b8c7509a5d987d256bf6e7ba5194f5c2916b
Alexey Borisov <a@borisco.ru>
2019-08-10 15:01:37 +0300

    added tests and controllers

---
+++ tests/booksControllerTests.js
@@ -0,0 +1,27 @@
+/* eslint-disable-next-line no-unused-vars */
+const should = require('should');
+const sinon = require('sinon');
+const booksController = require('../controllers/booksController');
+
+describe('Books Controller Tests', () => {
+	describe('Post', () => {
+		it('should not allow an empty title on post', () => {
+			/* eslint-disable-next-line no-unused-vars, func-names */
+			const Book = function (book) { this.save = () => { }; };
+			const req = {
+				body: {
+					author: 'Jon'
+				}
+			};
+			const res = {
+				status: sinon.spy(),
+				send: sinon.spy(),
+				json: sinon.spy()
+			};
+			const controller = booksController(Book);
+			controller.post(req, res);
+			res.status.calledWith(400).should.equal(true, `Bad status ${res.status.args[0][0]}`);
+			res.send.calledWith('Title is required').should.equal(true);
+		});
+	});
+});
